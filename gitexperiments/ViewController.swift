80886e6e209626347ea1524b722b302cd02740a7
Ishwar Kaushik <ishwar.kaushik@mindtree.com>
2017-04-17 16:28:50 +0530

    Hello World

--- gitexperiments/ViewController.swift
+++ gitexperiments/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("WE are in the view Did Load method in the ViewController")
     }
 
     override func didReceiveMemoryWarning() {
