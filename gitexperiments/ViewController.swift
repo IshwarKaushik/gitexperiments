8df469eef28ab115e4baadaea71f158e1b791d83
Ishwar Kaushik <ishwar.kaushik@mindtree.com>
2017-04-17 16:14:52 +0530

    Initial Commit

---
+++ gitexperiments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitexperiments
+//
+//  Created by Ishwar Kaushik on 17/04/17.
+//  Copyright © 2017 Ishwar Kaushik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
